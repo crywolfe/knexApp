78267e0e6c562a87f4d221397d9d986ee7997dd1
crywolfe <gwolfe2007@gmail.com>
2014-09-22 20:22:32 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+    knex = require('knex'),
+    bookshelf = require('bookshelf'),
+    app = express();
+
+//db setup
+var db_config = knex(require('./knexfile').development);
+var bookshelf = bookshelf(db_config);
+app.set('bookshelf', bookshelf);
+
+//user model setup
+var User = bookshelf.Model.extend({
+  tableName: 'users'
+});
+
+// routes
+
+app.get('/', function(req, res){
+  User.fetchAll().then(function(all){res.send(all); });
+});
+
+app.get('/new', function(req, res){
+  var user = new User({ email: 'foo@bar.com' });
+  user.save().then(function(){res.send(user);});
+});
+
+var server = app.listen(3000, function(){
+  console.log("listening on port %d", server.address().port);
+});
---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'MB'
+
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'postgresql'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'postgresql'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'postgresql'
+    }
+  }
+
+};
